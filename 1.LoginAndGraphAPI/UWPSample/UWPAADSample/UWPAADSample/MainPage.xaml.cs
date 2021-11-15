43a820b0546a418ae3abb73b3d1c26fd7c3afeb1
Wayne Wang <waywa@microsoft.com>
2021-11-15 15:36:42 +0800

    Worked for graph call

--- 1.LoginAndGraphAPI/UWPSample/UWPAADSample/UWPAADSample/MainPage.xaml.cs
+++ 1.LoginAndGraphAPI/UWPSample/UWPAADSample/UWPAADSample/MainPage.xaml.cs
@@ -8,6 +8,11 @@ using Microsoft.Graph;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Credentials;
+using Windows.Security.Authentication.Web.Core;
+using System.Text;
+using System.IO;
 
 namespace UWPAADSample
 {
@@ -23,9 +28,11 @@ namespace UWPAADSample
         // Below are the clientId (Application Id) of your app registration and the tenant information.
         // You have to replace:
         // - the content of ClientID with the Application Id for your app registration
-        private const string ClientId = "d76b333f-f49d-455e-b2eb-1110f9c21a19";
+        private const string ClientId = "da357db4-ae75-4d9d-bf1c-520a8e7d4a40";
+        //private const string ClientId = "d76b333f-f49d-455e-b2eb-1110f9c21a19";
 
-        private const string Tenant = "AACMRTestAAD1outlook.onmicrosoft.com"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
+        private const string Tenant = "microsoft.onmicrosoft.com";//
+        //private const string Tenant = "AACMRTestAAD1outlook.onmicrosoft.com"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
         private const string Authority = "https://login.microsoftonline.com/" + Tenant;
 
         // The MSAL Public client app
@@ -48,6 +55,21 @@ namespace UWPAADSample
                 new DelegateAuthenticationProvider(async (requestMessage) =>
                 {
                     requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingMSAL(scopes));
+                }));
+
+            return graphClient;
+        }
+
+        /// <summary>
+        /// Sign in user using MSAL and obtain a token for Microsoft Graph
+        /// </summary>
+        /// <returns>GraphServiceClient</returns>
+        private async static Task<GraphServiceClient> SignInAndInitializeGraphServiceClientWAML(string[] scopes)
+        {
+            GraphServiceClient graphClient = new GraphServiceClient(MSGraphURL,
+                new DelegateAuthenticationProvider(async (requestMessage) =>
+                {
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingWAML(scopes[0]));
                 }));
 
             return graphClient;
@@ -132,6 +154,39 @@ namespace UWPAADSample
             }
         }
         /// <summary>
+        /// Call AcquireTokenAsync - to acquire a token requiring user to sign in
+        /// </summary>
+        private async void CallGraphButtonWAML_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Sign in user using MSAL and obtain an access token for Microsoft Graph
+                GraphServiceClient graphClient = await SignInAndInitializeGraphServiceClientWAML(scopes);
+
+                // Call the /me endpoint of Graph
+                User graphUser = await graphClient.Me.Request().GetAsync();
+
+                // Go back to the UI thread to make changes to the UI
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    ResultText.Text = "Display Name: " + graphUser.DisplayName + "\nBusiness Phone: " + graphUser.BusinessPhones.FirstOrDefault()
+                                      + "\nGiven Name: " + graphUser.GivenName + "\nid: " + graphUser.Id
+                                      + "\nUser Principal Name: " + graphUser.UserPrincipalName;
+                    DisplayBasicTokenInfo(authResult);
+                    this.SignOutButton.Visibility = Visibility.Visible;
+                });
+            }
+            catch (MsalException msalEx)
+            {
+                await DisplayMessageAsync($"Error Acquiring Token:{System.Environment.NewLine}{msalEx}");
+            }
+            catch (Exception ex)
+            {
+                await DisplayMessageAsync($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
+                return;
+            }
+        }
+        /// <summary>
         /// Signs in the user and obtains an access token for Microsoft Graph
         /// </summary>
         /// <param name="scopes"></param>
@@ -141,7 +196,7 @@ namespace UWPAADSample
             // Initialize the MSAL library by building a public client application
             PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
                 .WithAuthority(Authority)
-                .WithUseCorporateNetwork(false)
+                .WithUseCorporateNetwork(true)
                 .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
                  .WithLogging((level, message, containsPii) =>
                  {
@@ -156,19 +211,186 @@ namespace UWPAADSample
             try
             {
                 authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+                    
                                                   .ExecuteAsync();
             }
             catch (MsalUiRequiredException ex)
             {
                 // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
                 Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
-
-                authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
-                                                  .ExecuteAsync()
-                                                  .ConfigureAwait(false);
+           
+             
+                    authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+                    
+                                                .ExecuteAsync()
+                                                .ConfigureAwait(false);
+                
 
             }
             return authResult.AccessToken;
         }
+
+        /// <summary>
+        /// Signs in the user and obtains an access token for Microsoft Graph
+        /// </summary>
+        /// <param name="scopes"></param>
+        /// <returns> Access Token</returns>
+        private static async Task<string> SignInUserAndGetTokenUsingWAML(string scopes)
+        {
+            var BiometricsRequired = true;
+            // Initialize the MSAL library by building a public client application
+            string accessToken = string.Empty;
+
+            if (BiometricsRequired)
+            {
+                if (await Windows.Security.Credentials.UI.UserConsentVerifier.CheckAvailabilityAsync() == Windows.Security.Credentials.UI.UserConsentVerifierAvailability.Available)
+                {
+                    var consentResult = await Windows.Security.Credentials.UI.UserConsentVerifier.RequestVerificationAsync("Please verify your credentials");
+                    if (consentResult != Windows.Security.Credentials.UI.UserConsentVerificationResult.Verified)
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+            string URI = string.Format("ms-appx-web://Microsoft.AAD.BrokerPlugIn/{0}",
+                WebAuthenticationBroker.GetCurrentApplicationCallbackUri().Host.ToUpper());
+
+            WebAccountProvider wap =
+                await WebAuthenticationCoreManager.FindAccountProviderAsync("https://login.microsoft.com", Authority);
+
+
+
+
+            WebTokenRequest wtr = new WebTokenRequest(wap, "https://graph.microsoft.com/.default", ClientId);
+            wtr.Properties.Add("resource", "https://graph.microsoft.com/.default");
+
+            WebAccount account = null;
+
+            //if (!string.IsNullOrEmpty((string)userId))
+            //{
+            //    account = await WebAuthenticationCoreManager.FindAccountAsync(wap, (string)userId);
+            //    if (account != null)
+            //    {
+            //        Logger.Log("Found account: " + account.UserName);
+            //    }
+            //    else
+            //    {
+            //        Logger.Log("Account not found");
+            //    }
+            //}
+
+            WebTokenRequestResult tokenResponse = null;
+            try
+            {
+                if (account != null)
+                {
+                    tokenResponse = await WebAuthenticationCoreManager.GetTokenSilentlyAsync(wtr, account);
+                }
+                else
+                {
+                    tokenResponse = await WebAuthenticationCoreManager.GetTokenSilentlyAsync(wtr);
+                    //tokenResponse = await WebAuthenticationCoreManager.RequestTokenAsync(wtr);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //Logger.Log(ex.Message);
+            }
+
+            if (tokenResponse.ResponseError != null)
+            {
+
+                var sb = new StringBuilder("ResponseError:");
+                foreach (var errProp in tokenResponse.ResponseError.Properties)
+                {
+                    sb.AppendLine($"Error prop: ({errProp.Key}, {errProp.Value})");
+                }
+                throw new Exception(sb.ToString());
+            }
+
+            if (tokenResponse.ResponseStatus == WebTokenRequestStatus.UserInteractionRequired)
+            {
+                WebTokenRequestResult wtrr = null;
+                try
+                {
+                    if (account != null)
+                        wtrr = await WebAuthenticationCoreManager.RequestTokenAsync(wtr, account);
+                    else
+                        wtrr = await WebAuthenticationCoreManager.RequestTokenAsync(wtr);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+                if (wtrr.ResponseError != null)
+                {
+                    var sb = new StringBuilder("ResponseError:");
+
+                    sb.AppendLine("Error Code: " + wtrr.ResponseError.ErrorCode.ToString());
+                    sb.AppendLine("Error Msg: " + wtrr.ResponseError.ErrorMessage.ToString());
+                    foreach (var errProp in wtrr.ResponseError.Properties)
+                    {
+                        sb.AppendLine($"Error prop: ({errProp.Key}, {errProp.Value})");
+                    }
+                    throw new Exception(sb.ToString());
+                }
+
+                if (wtrr.ResponseStatus == WebTokenRequestStatus.Success)
+                {
+                    accessToken = wtrr.ResponseData[0].Token;
+                    account = wtrr.ResponseData[0].WebAccount;
+                    var properties = wtrr.ResponseData[0].Properties;
+                    //Username = account.UserName;
+                    //Logger.Log($"Username = {Username}");
+                    var ras = await account.GetPictureAsync(WebAccountPictureSize.Size64x64);
+                    var stream = ras.AsStreamForRead();
+                    var br = new BinaryReader(stream);
+                    //UserPicture = br.ReadBytes((int)stream.Length);
+
+                    //Logger.Log("Access Token: " + accessToken, false);
+                }
+            }
+
+            if (tokenResponse.ResponseStatus == WebTokenRequestStatus.Success)
+            {
+                foreach (var resp in tokenResponse.ResponseData)
+                {
+                    var name = resp.WebAccount.UserName;
+                    accessToken = resp.Token;
+                    account = resp.WebAccount;
+                    //Username = account.UserName;
+                    //Logger.Log($"Username = {Username}");
+                    try
+                    {
+                        var ras = await account.GetPictureAsync(WebAccountPictureSize.Size64x64);
+                        var stream = ras.AsStreamForRead();
+                        var br = new BinaryReader(stream);
+                        //UserPicture = br.ReadBytes((int)stream.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                        //Logger.Log($"Exception when reading image {ex.Message}");
+                    }
+                }
+
+                //Logger.Log("Access Token: " + accessToken, false);
+            }
+
+            //if (account != null && !string.IsNullOrEmpty(account.Id))
+            //{
+            //    Store.SaveUser(UserIdKey, account.Id);
+            //}
+            //#endif
+            return accessToken;
+        }
     }
+
 }
--- 1.LoginAndGraphAPI/UnitySample/Assets/MainController.cs
+++ 1.LoginAndGraphAPI/UnitySample/Assets/MainController.cs
@@ -1,5 +1,4 @@
 
-#define ENABLE_UWP_CODE
 
 using System.Collections;
 using System.Collections.Generic;
@@ -7,13 +6,11 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using System;
-#if ENABLE_UWP_CODE
 using System.Linq;
-//using Microsoft.Identity.Client;
+using Microsoft.Identity.Client;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 
-#endif
 
 public class MainController : MonoBehaviour
 {
@@ -26,94 +23,129 @@ public class MainController : MonoBehaviour
     public string Tenant;
     public string Authority => "https://login.microsoftonline.com/" + Tenant;
     public string MSGraphURL = "https://graph.microsoft.com/v1.0/";
-#if ENABLE_UWP_CODE
-    //private static IPublicClientApplication PublicClientApp;
-    //private static AuthenticationResult authResult;
-    //private void DisplayBasicTokenInfo(AuthenticationResult authResult)
-    //{
-    //    TokenDetailText.text = "";
-    //    if (authResult != null)
-    //    {
-    //        TokenDetailText.text += $"User Name: {authResult.Account.Username}" + Environment.NewLine;
-    //        TokenDetailText.text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
-    //    }
-    //}
-#endif
+    private static IPublicClientApplication PublicClientApp;
+    private static AuthenticationResult authResult;
+    private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+    {
+        TokenDetailText.text = "";
+        if (authResult != null)
+        {
+            TokenDetailText.text += $"User Name: {authResult.Account.Username}" + Environment.NewLine;
+            TokenDetailText.text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+        }
+    }
+
 
 
     private async Task DisplayMessageAsync(string message)
     {
         CallResultText.text = message;
     }
-    private async Task<string> SignInUserAndGetTokenUsingMSAL(string[] scopes)
+    private async Task<AuthenticationResult> SignInUserAndGetTokenUsingMSAL(string[] scopes)
     {
-#if ENABLE_UWP_CODE
-        var outString = await IdentityClientPlugin.IdentityClient.SignInUserAndGetTokenUsingMSAL(scopes);
-        return outString;
-        //PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
-        //           .WithAuthority(Authority)
-        //           .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
-        //            .WithLogging((level, message, containsPii) =>
-        //            {
-        //                System.Diagnostics.Debug.WriteLine($"MSAL: {level} {message} ");
-        //            }, LogLevel.Warning, enablePiiLogging: false, enableDefaultPlatformLogging: true)
-        //           .Build();
 
-        //// It's good practice to not do work on the UI thread, so use ConfigureAwait(false) whenever possible.
-        //IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
-        //IAccount firstAccount = accounts.FirstOrDefault();
-
-        //try
-        //{
-        //    authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
-        //                                      .ExecuteAsync();
-        //}
-        //catch (MsalUiRequiredException ex)
-        //{
-        //    // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
-        //    System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
-
-        //    authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
-        //                                      .ExecuteAsync()
-        //                                      .ConfigureAwait(false);
-
-        //}
-        //return authResult.AccessToken;
-#else
-        return "This_Is_A_Fake_Token_For_Editor_Player";
+        PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                    .WithAuthority(Authority)
+#if ENABLE_WINMD_SUPPORT
+                    .WithUseCorporateNetwork(true)
 #endif
+                    .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                    .WithLogging((level, message, containsPii) =>
+                        {
+                            System.Diagnostics.Debug.WriteLine($"MSAL: {level} {message} ");
+                        }, 
+                        LogLevel.Warning, enablePiiLogging: false, enableDefaultPlatformLogging: true)
+                    .Build();
+
+        // It's good practice to not do work on the UI thread, so use ConfigureAwait(false) whenever possible.
+        IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(true);
+        IAccount firstAccount = accounts.FirstOrDefault();
+
+        try
+        {
+            authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+                                              .ExecuteAsync();
+        }
+        catch (MsalUiRequiredException ex)
+        {
+            // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
+            System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+            try
+            {
+                authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+                                                  .ExecuteAsync()
+                                                  .ConfigureAwait(true);
+            }
+            catch (Exception exIntactive)
+            {
+                var msg = $"{exIntactive.GetType().Name}: {exIntactive.Message}";
+                System.Diagnostics.Debug.WriteLine(exIntactive);
+                await DisplayMessageAsync(msg);
+                throw;
+            }
+        }
+        return authResult;
+
+    }
+
+    /// <summary>
+    /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+    /// </summary>
+    /// <param name="url">The URL</param>
+    /// <param name="token">The token</param>
+    /// <returns>String containing the results of the GET operation</returns>
+    public async Task<string> GetHttpContentWithToken(string url, string token)
+    {
+        var httpClient = new System.Net.Http.HttpClient();
+        System.Net.Http.HttpResponseMessage response;
+        try
+        {
+            var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+            //Add the token in Authorization header
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            response = await httpClient.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+        catch (Exception ex)
+        {
+            return ex.ToString();
+        }
     }
     public async void SignOutButton_Click()
     {
-#if ENABLE_UWP_CODE
-        //IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
-        //IAccount firstAccount = accounts.FirstOrDefault();
 
-        //try
-        //{
-        //    await PublicClientApp.RemoveAsync(firstAccount).ConfigureAwait(false);
-        //    CallResultText.text = "User has signed out";
-        //    this.CallingGraphButton.SetActive(true);
-        //    this.SignOutButton.SetActive(false);
-        //}
-        //catch (MsalException ex)
-        //{
-        //    await DisplayMessageAsync($"Error signing out user: {ex.Message}");
-        //}
-#else
-        CallResultText.text = "This is a fake signout";
-        SignOutButton?.SetActive(false);
-#endif
+        IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(true);
+        IAccount firstAccount = accounts.FirstOrDefault();
+
+        try
+        {
+            await PublicClientApp.RemoveAsync(firstAccount).ConfigureAwait(true);
+            CallResultText.text = "User has signed out";
+            this.CallingGraphButton.SetActive(true);
+            this.SignOutButton.SetActive(false);
+        }
+        catch (MsalException ex)
+        {
+            await DisplayMessageAsync($"Error signing out user: {ex.Message}");
+        }
+
     }
     public async void CallGraphButton_Click()
     {
-#if ENABLE_UWP_CODE
-      var outstring=  await SignInUserAndGetTokenUsingMSAL(scopes);
-        await DisplayMessageAsync(outstring);
+        try
+        {
+            var authResult = await SignInUserAndGetTokenUsingMSAL(scopes);
+            DisplayBasicTokenInfo(authResult);
+            SignOutButton.SetActive(true);
+            var outstring = await GetHttpContentWithToken("https://graph.microsoft.com/v1.0/me", authResult.AccessToken);
+            await DisplayMessageAsync(outstring);
+        }
+        catch (Exception ex)
+        {
+            await DisplayMessageAsync(ex.Message);
+        }
 
-#else
-        CallResultText.text = "This is a fake  success calling ";
-        SignOutButton?.SetActive(true);
-#endif
     }
 }
